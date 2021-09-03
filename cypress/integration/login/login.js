230509171d5e788050fb281792a3c46bcd9aae7e
Maurizio Miño - Kiru <kd.maurii@gmail.com>
2021-09-03 00:57:29 -0300

    Refactorizado para usar gherkin

--- cypress/integration/login/login.js
+++ cypress/integration/login/login.js
@@ -1,23 +1,34 @@
-describe('Probando el login', () => {
-    const email = "zxc@gmail.com";
-    const password = "12345";
-    const username = "zxc zcx";
+const email = "zxc@gmail.com";
+const password = "12345";
+const username = "zxc zcx";
 
-    it('Realizando login exitoso', () => {
-        cy.visit('/');
-        cy.get(".login").click();
-        cy.fixture('home').then( home => {
-            cy.get(home.login).click();
-        });
-        cy.fixture('login').then( login => {
-            cy.get(login.email).type(email);
-            cy.get(login.password).type(password);
-            cy.get(login.signInButton).click();            
-        });
-        cy.fixture('myAccount').then( myAccount => {
-            cy.get(myAccount.myUser).should('contain', username);
-        })
 
+Given("Estoy en la pagina principal", () => {
+  cy.visit("/");
+});
+
+When("Hago click en Sign In", () => {
+    cy.get(".login").click();
+    cy.fixture("home").then((home) => {
+        cy.get(home.login).click();
     });
+});
 
+And("Uso los datos predefinidos", () => {
+    cy.fixture('login').then( login => {
+        cy.get(login.email).type(email);
+        cy.get(login.password).type(password);
+    });
+});
+
+And("Hago click en el boton Sign in", () => {
+    cy.fixture('login').then( login => {
+        cy.get(login.signInButton).click();
+    });
+});
+
+Then("Veo la pantalla principal de mi cuenta", () => {
+    cy.fixture('myAccount').then( myAccount => {
+        cy.get(myAccount.myUser).should('contain', username);
+    })
 });
