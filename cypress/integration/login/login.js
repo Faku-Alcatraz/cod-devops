ec8afde9091c3eb5fcf97ed2811c0bf44f8f7e30
Maurizio Miño - Kiru <kd.maurii@gmail.com>
2021-09-02 23:00:21 -0300

    Primer caso básico de login

---
+++ cypress/integration/login/login.js
@@ -0,0 +1,20 @@
+describe('Probando el login', () => {
+    const email = "zxc@gmail.com";
+    const password = "12345";
+
+    it('Realizando login exitoso', () => {
+        cy.visit('/');
+        cy.get(".login").click();
+        cy.fixture('home').then( home => {
+            cy.get(home.login).click();
+        });
+        cy.fixture('login').then( login => {
+            cy.get(login.email).type(email);
+            cy.get(login.password).type(password);
+            cy.get(login.signInButton).click();
+            
+        });
+
+    });
+
+});
